a95abd1804fbdc685e901de5d77960656dddc21b
abdoi94 <abdoi94.iq@gmail.com>
2019-08-15 22:20:20 +0300

    AOSiP-ify
    Signed-off-by: abdoi94 <abdoi94.iq@gmail.com>

--- msm8976.mk
+++ msm8976.mk
@@ -21,7 +21,7 @@ $(call inherit-product, vendor/samsung/msm8976-common/msm8976-common-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosip
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
@@ -200,10 +200,6 @@ PRODUCT_PACKAGES += \
     android.hardware.light@2.0-impl \
     android.hardware.light@2.0-service \
     lights.MSM8952
-
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.samsung-qcom
 
 # Media
 PRODUCT_COPY_FILES += \
